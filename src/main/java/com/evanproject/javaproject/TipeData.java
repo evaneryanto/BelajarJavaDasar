44dea17899a8a3cf3dbfd589fb79c33276c3f55f
evaneryanto <evaneryanto95@gmail.com>
2022-12-18 20:41:49 +0700

    Commit Pertama

---
+++ src/main/java/com/evanproject/javaproject/TipeData.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.evanproject.javaproject;
+
+/**
+ *
+ * @author Bernardus Evan
+ */
+public class TipeData {
+    public static void main(String[] args) {
+        
+    }
+}
