bd896de8eb1acccd92da2d8e2e1f7e626b58e100
evaneryanto <evaneryanto95@gmail.com>
2022-12-18 22:37:23 +0700

    menambah konversi bilangan

--- src/main/java/com/evanproject/javaproject/TipeData.java
+++ src/main/java/com/evanproject/javaproject/TipeData.java
@@ -10,6 +10,16 @@ package com.evanproject.javaproject;
  */
 public class TipeData {
     public static void main(String[] args) {
+        int decimalInt = 25;
+        long angkaLong = 10000000;
+        int amount = 1_000_000_000;
+        
+        byte iniByte = 10;
+        short iniShort = iniByte;
+        int iniInteger = iniShort;
+        long iniLong = iniInteger;
+        float iniFloat = iniInteger;
+        
         
     }
 }
